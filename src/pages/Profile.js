09e7f622369e0e0d48d19e2f064f3bc555e29cb5
Ontorok <nasir.amath07@gmail.com>
2022-03-26 21:29:06 +0600

    profile add

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,32 @@
 import React from "react";
+import imgSrc from "../assets/images/img_avatar.png";
+import classes from "../styles/Profile.module.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className={classes.profileContainer}>
+      <div className={classes.profileLeft}>
+        <div className={classes.profileItem}>
+          <span>Name</span>
+          <span>:</span>
+          <span>Nasir Ahmed</span>
+        </div>
+        <div className={classes.profileItem}>
+          <span>Email</span>
+          <span>:</span>
+          <span>nasir@mail.com</span>
+        </div>
+        <div className={classes.profileItem}>
+          <span>Phone</span>
+          <span>:</span>
+          <span>+880 1x xxx-xxx</span>
+        </div>
+      </div>
+      <div className={classes.profileRight}>
+        <img src={imgSrc} alt="nasir" />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
