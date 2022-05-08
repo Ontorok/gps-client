5e9bbbf2b8616461f9f159cbe7a464ca58ae1fd7
Ontorok <nasir.amath07@gmail.com>
2022-05-08 15:50:43 +0600

    add heroko site

--- src/services/auth/jwt/config.js
+++ src/services/auth/jwt/config.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: `http://localhost:3500/api`, //YOUR_API_URL HERE
+  // baseURL: `http://localhost:3500/api`, //YOUR_API_URL HERE
+  baseURL: `https://gps-api-app.herokuapp.com/api`, //YOUR_API_URL HERE
   headers: {
     'Content-Type': 'application/json'
   }
