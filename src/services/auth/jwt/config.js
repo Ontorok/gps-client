85a3d478b807c7473bf3a3a9555c5c27aa280b5a
Ontorok <nasir.amath07@gmail.com>
2022-05-21 13:00:44 +0600

    base url comes from env file

--- src/services/auth/jwt/config.js
+++ src/services/auth/jwt/config.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: `http://localhost:3500/api` //YOUR_API_URL HERE
-  //baseURL: `https://gps-api-app.herokuapp.com/api`, //YOUR_API_URL HERE
+  baseURL: process.env.REACT_APP_BASE_URL //YOUR_API_URL HERE
 });
 
 export const privateAxiosInstance = axios.create({
-  baseURL: `http://localhost:3500/api`,
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   },
