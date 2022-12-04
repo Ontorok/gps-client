ff9882e897dc48f39cdfe9be510294b8bef79871
Nasir Ahmed <nasir.math07@gmail.com>
2022-12-04 10:56:45 +0600

    change origin

--- src/services/auth/jwt/config.js
+++ src/services/auth/jwt/config.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   //baseURL: process.env.REACT_APP_BASE_URL //YOUR_API_URL HERE
-  baseURL:  "http://ec2-3-83-240-253.compute-1.amazonaws.com/api"
+  baseURL:  "http://ec2-3-83-240-253.compute-1.amazonaws.com:4000/api"
 });
 
 export const privateAxiosInstance = axios.create({
   //baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: "http://ec2-3-83-240-253.compute-1.amazonaws.com/api",
+  baseURL: "http://ec2-3-83-240-253.compute-1.amazonaws.com:4000/api",
   headers: {
     'Content-Type': 'application/json'
   },
