a87940ba2744ac06d736ebfae1520cb257f6385b
Nasir Ahmed <nasir.math07@gmail.com>
2022-12-04 10:46:44 +0600

    skiped env file

--- src/services/auth/jwt/config.js
+++ src/services/auth/jwt/config.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL //YOUR_API_URL HERE
+  //baseURL: process.env.REACT_APP_BASE_URL //YOUR_API_URL HERE
+  baseURL:  "http://ec2-3-83-240-253.compute-1.amazonaws.com/api"
 });
 
 export const privateAxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  //baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "http://ec2-3-83-240-253.compute-1.amazonaws.com/api",
   headers: {
     'Content-Type': 'application/json'
   },
